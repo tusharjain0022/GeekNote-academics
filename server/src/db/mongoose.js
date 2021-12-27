986f7ffd3aecb64db8df892c1eb6755939bc5a1f
Sahil Gupta <sahilgupta.btech.cs19@iiitranchi.ac.in>
2021-12-27 23:48:17 +0530

    DB changes

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -2,12 +2,25 @@ const mongoose = require("mongoose");
 
 const uri = process.env.URI || "mongodb://localhost:27017/geeknote-academics"
 
-mongoose.connect(uri,{
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("database connection successfull");
-}).catch(()=>{
-    console.log("ERROR in database connection");
-})
+// mongoose.connect(uri,{
+//     useCreateIndex:true,
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// }).then(()=>{
+//     console.log("database connection successfull");
+// }).catch(()=>{
+//     console.log("ERROR in database connection");
+// })
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
